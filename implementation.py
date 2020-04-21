1aec99b625aa763a1f124e6d5524fcb32a6e71f7
Samarth <Athreya>
2020-04-21 15:18:33 -0400

    initialized parameters + built out forward prop

--- implementation.py
+++ implementation.py
@@ -1,5 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Step 1: Initialization of parameters
-def initialize_parameters(lenw)
+#Step 1: Initialization of parameters
+def initialize_parameters(lenw):
+    w = np.zeros((1, lenw))
+    b = 0
+    return w, b
+
+#Step 2: Forward Propagation
+def forward_prop(X, w, b)
+    z = np.dot(w,X) + b
+    return z
+
+#Step 3:
