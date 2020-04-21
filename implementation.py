54170ee0a740e37895bc7fcf94a3091436403f37
Samarth <Athreya>
2020-04-21 15:34:25 -0400

    implemented plotting

--- implementation.py
+++ implementation.py
@@ -31,3 +31,38 @@ def gradient_descent_update(w, b, dw, db, learning_rate):
     b = b - learning_rate*db
     return w, b
 
+#Step 6: Define Linear Regression Model
+def linear_reg_model(X_train, y_train, X_val, y_val, learning_rate, epochs):
+    lenw = X_train.shape[0]
+    w,b = initialize_parameters(lenw)
+
+    costs_train = []
+    m_train = y_train.shape[1]
+    m_val = y_val.shape[1]
+
+    for i in range(1, epochs+1):
+        z_train = forward_prop(X_train, w, b)
+        cost_train = cost_function(z_train,y_train)
+        dw,db = back_prop(X_train, y_train, z_train)
+        w,b = gradient_descent_update(w, b, dw, db, learning_rate)
+
+        #Storing training cost in a list to plot
+        if i%10==0:
+            costs_train.append(cost_train)
+        MAE_train = (1/m_train) np.sum(np.abs(z_train-y_train))
+
+        #Finding cost function value
+        z_val = forward_prop(X_val,w,b)
+        cost_val = cost_function(z_val, y_val)
+        MAE_train = (1/m_val)*np.sum(np.abs(z_train-y_train))
+
+        #Printing Values
+        print('Epochs' +str(i)+'/'+str(epochs)+': ')
+        print('Tech cost'+str(cost_train)+'|'+'Validation cost' +str(cost_val))
+        print('Tech cost'+str(MAE_train)+'|'+'Validation MAE' +str(MAE_val))
+
+    plt.plot(costs_train)
+    plt.xlabel('Iterations(per tens)')
+    plt.ylabel('Training Cost')
+    plt.title('Learning rate'+str(learning_rate))
+    plt.show()
